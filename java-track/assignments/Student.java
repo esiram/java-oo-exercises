65dcaf09fc15ae29a159451135694b85aa36f8a5
Esther Siram <isusecalea@yahoo.com>
2016-10-05 18:20:11 -0500

    commit wip: student class with constructor, initial getters, and getClassStanding function complete (need to test still with updated java-oo file from LC -es 10-5-16 18:20

--- java-track/assignments/Student.java
+++ java-track/assignments/Student.java
@@ -1,14 +1,15 @@
 
 public class Student {
 
-	//Fields:
+//Fields:
 	private String firstName;
 	private String lastName;
 	private int studentID;
 	private int credits;
 	private double gpa;
 	
-	//Behaviors (methods):
+	
+//Behaviors (methods):
 		//create constructor
 	public Student(String firstName, String lastName, int studentID, int credits, double gpa)
 	{
@@ -18,13 +19,15 @@ public class Student {
 		this.credits = credits;
 		this.gpa = gpa;   
 	}
+
 	
-		//create getters and setters per unit test
+//create getters and setters per unit test
 	public String getName()
 	{
 		String name = this.firstName + " " + this.lastName;
 		return name;
 	}
+	
 	
 	
 	public int getStudentID()
@@ -38,14 +41,39 @@ public class Student {
 		return this.credits;
 	}
 	
+	
 	public double getGPA()
 	{
 		return this.gpa;
 	}
 	
 	
-		//getClassStanding()
+	//should I add a spot for classStanding to go in the student constructor or student class? - es10/5/16
+	public void getClassStanding() //Should this return void or should it return classStanding?? AND which: (int credits) or ()?-es10/5/16  
+		{
+		if(this.credits < 30) 
+			{
+				String classStanding = "Freshman";
+			}
+		else if(this.credits >= 30 & this.credits < 60)
+			{
+				String classStanding = "Sophomore";
+			}
+		else if(this.credits >= 60 & this.credits < 90)
+			{
+				String classStanding = "Junior";
+			}
+		else //this.credits >= 90
+			{
+				String classStanding = "Senior";
+			}	
+	}
+	
+}
 		//submitGrade()
+	//public void submitGrade(int)
+	//{
+		
 	    //computeTuition()
 	    //createLegacy() -- note: this involves making a new constructor for the new generation student
 
@@ -60,9 +88,9 @@ public class Student {
 	
 	
 	
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+	//}
 
-}
+//}
