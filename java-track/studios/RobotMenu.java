79f9ce58f7c22bc48b807749ee1595c27e9444e6
Esther Siram <isusecalea@yahoo.com>
2016-10-17 15:44:10 -0500

    Robot menu instituted. - 10/17/16

--- java-track/studios/RobotMenu.java
+++ java-track/studios/RobotMenu.java
@@ -33,10 +33,74 @@ public class RobotMenu {
 		System.out.println("3. Move a robot.");
 		System.out.println("4. Rotate a robot.");
 		System.out.println("5. Compute distance between two robots.");
-		System.out.println("6. Exit program.")
-		
-		
+		System.out.println("6. Exit program.");
+		int selection = s.nextInt(); //calling scanner to use the next integer method; if user puts in a non integer higher than x (6 here), this will fail :(
+		while(selection < 0 || selection >6)
+		{
+			System.out.println("Invalid selction, please try agains:");
+			selection =  s.nextInt();
+		}
+		return selection;
+	}
+	
+	public void processInput(int selection) // keep process separate from verification of user input (see startMenu() function).
+	{
+		if (selection == 1)
+		{
+			createRobot();
+		}
+		else if(selection == 2)
+		{
+			displayRobots();
+		}
+		else if(selection == 3)
+		{
+			moveRobots();
+		}
+		else if(selection == 4)
+		{
+			rotateRobots();
+		}
+		else if(selection == 5)
+		{
+			determineDistanceRobots();
+		}
 		
 	}
+	
+	private void displayRobots()
+	{
+		for(int i = 0; i < robots.size(); i++)
+		{
+			System.out.println((i+1) + ".)" + robots.get(i));  // where i is the index of robots ( similar to robots[i], but calling a method this time)
+		}
+	}
+	
+//	private Robot moveRobots() 
+//	{
+//		System.out.println("How long should the robot move?");	
+//	}
+	
+	private void createRobot()
+	{
+		System.out.println("Please provide the robot's name.");
+		String name = s.next();   //is this the robot's to string method or is it a different method that's called by the scanner function?
+		
+		System.out.println("Please enter the robot's x coordinate.");
+		int x = s.nextInt();
+		
+		System.out.println("Please enter the robot's y coordinate.");
+		int y = s.nextInt();
+		
+		System.out.println("Please enter the robot's speed.");
+		int speed = s.nextInt();
+		
+
+		System.out.println("Please enter the robot's orientation (i.e. North, East, South, West).");
+		String orientation = s.next();
+		
+		robots.add(new Robot(name, x, y, speed, orientation));
+	}
+	
 
 }
