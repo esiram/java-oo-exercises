b9ddc0cdc6000fe743a8157ad5768cd284dec221
Esther Siram <isusecalea@yahoo.com>
2016-10-19 17:07:39 -0500

    WIP commit robot form choices with buttons

---
+++ java-track/studios/FormRobot.java
@@ -0,0 +1,81 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Color;
+
+public class FormRobot {
+
+	private JFrame frmInteractiveRobots;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FormRobot window = new FormRobot();
+					window.frmInteractiveRobots.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FormRobot() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmInteractiveRobots = new JFrame();
+		frmInteractiveRobots.setTitle("Interactive Robots");
+		frmInteractiveRobots.setBounds(100, 100, 450, 300);
+		frmInteractiveRobots.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmInteractiveRobots.getContentPane().setLayout(null);
+		
+		JList list = new JList();
+		list.setBounds(150, 30, 253, 199);
+		frmInteractiveRobots.getContentPane().add(list);
+		
+		JButton btnNewButton = new JButton("Create Robot");
+		btnNewButton.setForeground(Color.BLACK);
+		btnNewButton.setFont(new Font("Showcard Gothic", Font.PLAIN, 11));
+		btnNewButton.setBounds(28, 30, 112, 23);
+		frmInteractiveRobots.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Select Robot");
+		btnNewButton_1.setFont(new Font("Showcard Gothic", Font.PLAIN, 11));
+		btnNewButton_1.setBounds(28, 67, 112, 23);
+		frmInteractiveRobots.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Move Robot");
+		btnNewButton_2.setFont(new Font("Showcard Gothic", Font.PLAIN, 11));
+		btnNewButton_2.setBounds(28, 101, 112, 23);
+		frmInteractiveRobots.getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Rotate Robot");
+		btnNewButton_3.setFont(new Font("Showcard Gothic", Font.PLAIN, 11));
+		btnNewButton_3.setBounds(28, 139, 112, 23);
+		frmInteractiveRobots.getContentPane().add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Compute Robot Distance");
+		btnNewButton_4.setFont(new Font("Showcard Gothic", Font.PLAIN, 11));
+		btnNewButton_4.setBounds(28, 173, 112, 23);
+		frmInteractiveRobots.getContentPane().add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("Exit");
+		btnNewButton_5.setFont(new Font("Showcard Gothic", Font.PLAIN, 11));
+		btnNewButton_5.setBounds(28, 206, 112, 23);
+		frmInteractiveRobots.getContentPane().add(btnNewButton_5);
+	}
+}
