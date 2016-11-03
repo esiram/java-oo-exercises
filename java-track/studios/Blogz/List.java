4c3e42e671f07b211abcf77a3d688016f35e22c1
Esther Siram <isusecalea@yahoo.com>
2016-11-03 15:14:53 -0500

    User class updated

--- java-track/studios/Blogz/List.java
+++
@@ -1,5 +0,0 @@
-package Blogz;
-
-public interface List {
-	public User addUser(User user); //method name/action will change
-}
--- java-track/studios/Blogz/User.java
+++ java-track/studios/Blogz/User.java
@@ -2,73 +2,52 @@ package Blogz;
 import java.lang.Object; //for regular expressions
 import java.util.regex.Matcher;//for regular expressions
 import java.util.regex.Pattern;//for regular expressions
+import java.util.ArrayList; // array list is a kind of list
+import java.util.List; // list is an interface
 
 
 
 public class User {
-	private String username;  // should this be private?
-	private static String password;  // should this be private?  should this be here? trying to be non-hashed pw
-	private static String hashedPW;  // should this be private? should this be here?
-	
+	private String username; 
+	private String password; 
+	private String hashedPW;
+	static List<User> users = new ArrayList<User>();	
 	
 	//HashPassword:
-	private static String hashPassword(String password){
-		password = hashedPW;
-		return hashedPW;
+	private static String hashPassword(String pwd){  //TODO: hashing will happen in Blogz2
+		return pwd;
 	}
-
 
 	//Validate PW
-	private String isValidPassword(String hashedPW){
-		String unHashedPW = "";
-		String message = "";
-		if(unHashedPW == password){ //how about .equals?????  fix variables
-			message = "Congratulations, the password works!";
-		}
-		else{ // (unHashedPW != password)
-			message = "Please try a different password.  Thank you.";
-		}
-		return message;
+	private boolean isValidPassword(String pwd){
+		return User.hashPassword(pwd) == this.hashedPW;
 	}
 	
-	//Validate UserName  
-	//TODO: MOVE -- NOTE THAT THIS CHECK SHOULD HAPPEN IN THE CONSTRUCTOR 
-	//TODO: GET RegExpressions in the right place.
+	//Validate UserName
 	private static boolean isValidUsername(String username){
-		Pattern p = Pattern.compile("a*b");
-		Matcher m = p.matcher("aaab");
-		boolean b = m.matches();
-		return b;
-		//regular expression to use: [a-zA-Z][a-zA-Z0-9_-]{4,11} 
+		 Pattern p = Pattern.compile("[a-zA-Z][a-zA-Z0-9_-]{4,11}");
+		 Matcher m = p.matcher(username);
+		 return m.matches();
 	}	
 		
-		
-//ES' attempts before reading link re: patterns:		
-//		if(username.length() < 13 && username.length() > 3 && username[0].isalpha == true ){ //TODO: look up regular expressions per the directions
-//			return true;
-//		}
-//		else{
-//			return false; //TODO:THROW AN EXCEPTION HERE
-//		}
-
-	
-	
-	
-	
 	
 	//Constructor:
-	public User(String username, String hashedPW){  //constructor
-		this.username = username;
+	public User(String username, String hashedPW){
 		this.hashedPW = hashPassword(this.password);
+		if (!User.isValidUsername(username)){
+			throw new IllegalArgumentException("Bad username.");	
+		}
+		else{
+			this.username = username;
+		}
 	}
 	
 	public String getUserName(){
 		return this.username;
 	}
 
-	public String getHashedPassword(){
-		return User.hashedPW;
+	public String getPassword(){
+		return this.password;
 	}
-
 	
 }
