8cd18320099c6fcf9af2ba91ecd035317fb59623
Esther Siram <isusecalea@yahoo.com>
2016-11-03 14:29:55 -0500

    created list interface -- may need to delete this later (TBD)

---
+++ java-track/studios/Blogz/List.java
@@ -0,0 +1,5 @@
+package Blogz;
+
+public interface List {
+	public User addUser(User user); //method name/action will change
+}
--- java-track/studios/Blogz/User.java
+++ java-track/studios/Blogz/User.java
@@ -1,16 +1,23 @@
 package Blogz;
+import java.lang.Object; //for regular expressions
+import java.util.regex.Matcher;//for regular expressions
+import java.util.regex.Pattern;//for regular expressions
+
+
 
 public class User {
 	private String username;  // should this be private?
 	private static String password;  // should this be private?  should this be here? trying to be non-hashed pw
 	private static String hashedPW;  // should this be private? should this be here?
 	
+	
 	//HashPassword:
 	private static String hashPassword(String password){
 		password = hashedPW;
 		return hashedPW;
 	}
-	
+
+
 	//Validate PW
 	private String isValidPassword(String hashedPW){
 		String unHashedPW = "";
@@ -23,6 +30,31 @@ public class User {
 		}
 		return message;
 	}
+	
+	//Validate UserName  
+	//TODO: MOVE -- NOTE THAT THIS CHECK SHOULD HAPPEN IN THE CONSTRUCTOR 
+	//TODO: GET RegExpressions in the right place.
+	private static boolean isValidUsername(String username){
+		Pattern p = Pattern.compile("a*b");
+		Matcher m = p.matcher("aaab");
+		boolean b = m.matches();
+		return b;
+		//regular expression to use: [a-zA-Z][a-zA-Z0-9_-]{4,11} 
+	}	
+		
+		
+//ES' attempts before reading link re: patterns:		
+//		if(username.length() < 13 && username.length() > 3 && username[0].isalpha == true ){ //TODO: look up regular expressions per the directions
+//			return true;
+//		}
+//		else{
+//			return false; //TODO:THROW AN EXCEPTION HERE
+//		}
+
+	
+	
+	
+	
 	
 	//Constructor:
 	public User(String username, String hashedPW){  //constructor
@@ -37,6 +69,6 @@ public class User {
 	public String getHashedPassword(){
 		return User.hashedPW;
 	}
-	
+
 	
 }
