ea168b0043e5fd42db0013cda890d03273dfbdf6
Esther Siram <isusecalea@yahoo.com>
2016-11-03 12:30:48 -0500

    Attempts at setting the PW as Hashed PW

--- java-track/studios/Blogz/User.java
+++ java-track/studios/Blogz/User.java
@@ -2,19 +2,41 @@ package Blogz;
 
 public class User {
 	private String username;  // should this be private?
-	private String hashedPW;  // should this be private?
+	private static String password;  // should this be private?  should this be here? trying to be non-hashed pw
+	private static String hashedPW;  // should this be private? should this be here?
 	
-	public User(String username, String hashedPW){
+	//HashPassword:
+	private static String hashPassword(String password){
+		password = hashedPW;
+		return hashedPW;
+	}
+	
+	//Validate PW
+	private String isValidPassword(String hashedPW){
+		String unHashedPW = "";
+		String message = "";
+		if(unHashedPW == password){ //how about .equals?????  fix variables
+			message = "Congratulations, the password works!";
+		}
+		else{ // (unHashedPW != password)
+			message = "Please try a different password.  Thank you.";
+		}
+		return message;
+	}
+	
+	//Constructor:
+	public User(String username, String hashedPW){  //constructor
 		this.username = username;
-		this.hashedPW = hashedPW;
+		this.hashedPW = hashPassword(this.password);
 	}
 	
 	public String getUserName(){
 		return this.username;
 	}
-	
-	public String getHashedPW(){
-		return this.hashedPW;
-	}
 
+	public String getHashedPassword(){
+		return User.hashedPW;
+	}
+	
+	
 }
