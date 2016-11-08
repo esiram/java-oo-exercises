73f0b58c1bce704b932de163c0eb0b2e164471ff
Esther Siram <isusecalea@yahoo.com>
2016-11-08 17:19:30 -0600

    JUnit test 1 works, but not test 2

--- java-track/studios/Blogz/TestBlogz.java
+++ java-track/studios/Blogz/TestBlogz.java
@@ -9,7 +9,13 @@ public class TestBlogz {
 	
 	@Test
 	public void testIsValidUsername() {
+		assertTrue(a.getUserName() != "appl*");
 		assertTrue(a.getUserName() == "apple");
 	}
+	
+//	@Test  //this test not working yet-ES11-8-16
+//	public void testIsValidPassword(){
+//		assertTrue(a.getPassword() == "jonathan");
+//	}
 
 }
