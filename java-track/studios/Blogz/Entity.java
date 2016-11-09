1752e529739ab2cf78b19af4174e37b03ce83c4c
Esther Siram <isusecalea@yahoo.com>
2016-11-09 15:43:56 -0600

    Create Entity Class - attempt

--- java-track/studios/Blogz/Entity.java
+++ java-track/studios/Blogz/Entity.java
@@ -1,22 +1,17 @@
 package Blogz;
 
 public abstract class Entity {
-	private final int uid;
+	private final int UID;
 	
-	//constructor
-	public Entity(int uid){
-//		while(this.uid > 0 && this.uid != other.uid){  // how do you get it so that it doesn't represent another uid????  How do you call the other?
-//		this.uid = uid;
-//		}
-//		else{
-//			this.uid = uid++;
-//		}
-//	}
+	//Constructor (private and final)
+	private Entity(int UID){
+		this.UID = UID;
+	}
 	
 	public int getUID(){
-		return this.uid;
+		return this.UID;
 	}
-	}
+}
 	
 	
 
